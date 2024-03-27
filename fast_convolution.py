fd96427a6019cd15a91f23b27d31a9ca36e21baa
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2024-03-26 22:22:36 -0300

    trabalhando no loop da convolução e adicionei imagens

--- fast_convolution.py
+++ fast_convolution.py
@@ -51,6 +51,85 @@ class C3x3_5m20a9e():
         out = self.a.T * self.gs * self.c.T * sy.Matrix(fv)
 
 
+def c3x3_5m20a9e(gv):
+    '''
+    From Blahut page 166
+    Linear, 3x3, 5 multiplications, 20 aditions and 9 extra operations
+    :return:
+    '''
+    _rin = 5
+    _rout = 3
+    _a = [
+        [1, 0, 0],
+        [1, 1, 1],
+        [1, -1, 1],
+        [1, 2, 4],
+        [0, 0, 1]
+    ]
+    _b = _a
+    _n = [[1, 2], [-1, 2], [-1, 6], [1, 6], [1, 1]]
+    _c = [
+        [2, 0, 0, 0, 0],
+        [-1, -2, 2, -1, 2],
+        [-2, -1, -3, 0, -1],
+        [1, 1, 1, 1, -2],
+        [0, 0, 0, 0, 1]
+    ]
+
+    a = sy.Matrix(_a)
+    b = sy.Matrix(_b)
+    c = sy.Matrix(_c)
+    n = sy.Matrix([sy.Rational(d, q) for d, q in _n])
+
+    g = sy.Matrix(sy.symbols(" ".join(f"g_{i}" for i in range(_rout))))
+    # f = sy.Matrix(sy.symbols(" ".join(f"f_{i}" for i in range(_rin))))
+    bg = sy.diag(*(b * g).tolist())
+    bgn = sy.diag(*(bg * n))
+    subs = {k: v for k, v in zip(g.values(), gv)}
+    gs = bgn.subs(subs)
+    # s = sy.MatMul(a.T, gs, c.T, f)
+    return wrap_convolution(c, gs, a)
+
+
+def c3x3_6m10a0e(gv):
+    '''
+    From Blahut page 164
+    Linear, 3x3, 6 multiplications, 10 aditions and 0 extra operations
+    :return:
+    '''
+    _rin = 5
+    _rout = 3
+    _a = [
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1],
+        [1, 1, 0],
+        [1, 0, 1],
+        [0, 1, 1]
+    ]
+    _b = _a
+    _c = [
+        [2, 0, 0, 0, 0],
+        [-1, -2, 2, -1, 2],
+        [-2, -1, -3, 0, -1],
+        [1, 1, 1, 1, -2],
+        [0, 0, 0, 0, 1]
+    ]
+    _n = [1, 1, 1, 1, 1, 1]
+
+    a = sy.Matrix(_a)
+    b = sy.Matrix(_b)
+    c = sy.Matrix(_c)
+    n = sy.Matrix([i for i in _n])
+
+    g = sy.Matrix(sy.symbols(" ".join(f"g_{i}" for i in range(_rout))))
+    bg = sy.diag(*(b * g).tolist())
+    bgn = sy.diag(*(bg * n))
+    subs = {k: v for k, v in zip(g.values(), gv)}
+    gs = bgn.subs(subs)
+    return wrap_convolution(c, gs, a)
+
+
 def recursive_log2(n):
     def _recursive_log2(n):
         return [e for e, b in enumerate(bin(n)[2::][::-1]) if b == '1']
@@ -125,94 +204,16 @@ def log2_matrix(lst):
     return mtx
 
 
-def wrap_convolution(a, bg, c):
-    def convolution(fv):
-        out = wrap_filter(a, bg, c) * sy.Matrix(fv)
+def wrap_convolution(c, bg, a):
+    filt = to_filter(c, bg, a)
+    def convolution(f):
+        out = filt * sy.Matrix(f)
         return out
     return convolution
 
 
-def wrap_filter(a, bg, c):
+def to_filter(c, bg, a):
     return a.T * bg * c.T
-
-
-def c3x3_5m20a9e(gv):
-    '''
-    From Blahut page 166
-    Linear, 3x3, 5 multiplications, 20 aditions and 9 extra operations
-    :return:
-    '''
-    _rin = 5
-    _rout = 3
-    _a = [
-        [1, 0, 0],
-        [1, 1, 1],
-        [1, -1, 1],
-        [1, 2, 4],
-        [0, 0, 1]
-    ]
-    _b = _a
-    _n = [[1, 2], [-1, 2], [-1, 6], [1, 6], [1, 1]]
-    _c = [
-        [2, 0, 0, 0, 0],
-        [-1, -2, 2, -1, 2],
-        [-2, -1, -3, 0, -1],
-        [1, 1, 1, 1, -2],
-        [0, 0, 0, 0, 1]
-    ]
-
-    a = sy.Matrix(_a)
-    b = sy.Matrix(_b)
-    c = sy.Matrix(_c)
-    n = sy.Matrix([sy.Rational(d, q) for d, q in _n])
-
-    g = sy.Matrix(sy.symbols(" ".join(f"g_{i}" for i in range(_rout))))
-    # f = sy.Matrix(sy.symbols(" ".join(f"f_{i}" for i in range(_rin))))
-    bg = sy.diag(*(b * g).tolist())
-    bgn = sy.diag(*(bg * n))
-    subs = {k: v for k, v in zip(g.values(), gv)}
-    gs = bgn.subs(subs)
-    # s = sy.MatMul(a.T, gs, c.T, f)
-    return wrap_convolution(a, gs, c)
-
-
-def c3x3_6m10a0e(gv):
-    '''
-    From Blahut page 164
-    Linear, 3x3, 6 multiplications, 10 aditions and 0 extra operations
-    :return:
-    '''
-    _rin = 5
-    _rout = 3
-    _a = [
-        [1, 0, 0],
-        [0, 1, 0],
-        [0, 0, 1],
-        [1, 1, 0],
-        [1, 0, 1],
-        [0, 1, 1]
-    ]
-    _b = _a
-    _c = [
-        [2, 0, 0, 0, 0],
-        [-1, -2, 2, -1, 2],
-        [-2, -1, -3, 0, -1],
-        [1, 1, 1, 1, -2],
-        [0, 0, 0, 0, 1]
-    ]
-    _n = [1, 1, 1, 1, 1, 1]
-
-    a = sy.Matrix(_a)
-    b = sy.Matrix(_b)
-    c = sy.Matrix(_c)
-    n = sy.Matrix([i for i in _n])
-
-    g = sy.Matrix(sy.symbols(" ".join(f"g_{i}" for i in range(_rout))))
-    bg = sy.diag(*(b * g).tolist())
-    bgn = sy.diag(*(bg * n))
-    subs = {k: v for k, v in zip(g.values(), gv)}
-    gs = bgn.subs(subs)
-    return wrap_convolution(a, gs, c)
 
 
 def toom_cook(d_size, g_size, points):
@@ -263,3 +264,12 @@ def toom_cook(d_size, g_size, points):
 
 def g2bg(cq, b, g):
     return sy.diag(*(sy.diag(*cq) * b * sy.Matrix(g)).tolist())
+
+
+def toom_cook_conv_1d(d_size, g_size, points, g):
+    c, cq, b, a = toom_cook(d_size, g_size, points)
+    bg = g2bg(cq, b, g)
+    f = wrap_convolution(c, bg, a)
+    return f
+
+
