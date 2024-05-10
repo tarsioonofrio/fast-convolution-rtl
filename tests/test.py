23ac5bbc5d1d9c9ed5995fe9cfa3d6911413e120
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2024-05-10 19:15:23 -0300

    add tests

--- tests/test.py
+++ tests/test.py
@@ -30,20 +30,20 @@ def test_init1d():
     assert result.returncode == 0
 
 
-def test_build_1d_toomcook():
-    result = subprocess.run(
-        ['fast-conv', 'build', '1d', 'toom-cook'],
-        capture_output=True,
-        cwd=tmp_dir1
-    )
-    assert result.returncode == 0
-
-
 def test_init2d():
     result = subprocess.run(
         ['fast-conv', 'init', '2d', '-o', '3', '3'],
         capture_output=True,
         cwd=tmp_dir2
+    )
+    assert result.returncode == 0
+
+
+def test_build_1d_toomcook():
+    result = subprocess.run(
+        ['fast-conv', 'build', '1d', 'toom-cook'],
+        capture_output=True,
+        cwd=tmp_dir1
     )
     assert result.returncode == 0
 
@@ -56,3 +56,130 @@ def test_build_2d_toom_cook():
     )
     assert result.returncode == 0
 
+
+def test_build_2d_toom_cook():
+    result = subprocess.run(
+        ['fast-conv', 'build', '2d', 'bind', 'iterate'],
+        capture_output=True,
+        cwd=tmp_dir2
+    )
+    assert result.returncode == 0
+
+
+def test_build_2d_toom_cook():
+    result = subprocess.run(
+        ['fast-conv', 'build', '2d', 'bind', 'nest'],
+        capture_output=True,
+        cwd=tmp_dir2
+    )
+    assert result.returncode == 0
+
+
+def test_quant_1d_shift():
+    result = subprocess.run(
+        ['fast-conv', 'quant', 'shift'],
+        capture_output=True,
+        cwd=tmp_dir1
+    )
+    assert result.returncode == 0
+
+
+def test_quant_2d_shift():
+    result = subprocess.run(
+        ['fast-conv', 'quant', 'shift'],
+        capture_output=True,
+        cwd=tmp_dir2
+    )
+    assert result.returncode == 0
+
+
+def test_example_1d_seq():
+    result = subprocess.run(
+        ['fast-conv', 'example', 'seq'],
+        capture_output=True,
+        cwd=tmp_dir1
+    )
+    assert result.returncode == 0
+
+
+def test_example_1d_rand():
+    result = subprocess.run(
+        ['fast-conv', 'example', ' rand'],
+        capture_output=True,
+        cwd=tmp_dir1
+    )
+    assert result.returncode == 0
+
+
+def test_example_2d_seq():
+    result = subprocess.run(
+        ['fast-conv', 'example', 'seq'],
+        capture_output=True,
+        cwd=tmp_dir2
+    )
+    assert result.returncode == 0
+
+
+def test_example_2d_rand():
+    result = subprocess.run(
+        ['fast-conv', 'example', ' rand'],
+        capture_output=True,
+        cwd=tmp_dir2
+    )
+    assert result.returncode == 0
+
+
+
+
+def test_sim_1d_seq():
+    result = subprocess.run(
+        ['fast-conv', 'sim', 'file'],
+        capture_output=True,
+        cwd=tmp_dir1
+    )
+    assert result.returncode == 0
+
+
+def test_sim_1d_rand():
+    result = subprocess.run(
+        ['fast-conv', 'sim', ' rand'],
+        capture_output=True,
+        cwd=tmp_dir1
+    )
+    assert result.returncode == 0
+
+
+def test_example_2d_seq():
+    result = subprocess.run(
+        ['fast-conv', 'sim', 'seq'],
+        capture_output=True,
+        cwd=tmp_dir2
+    )
+    assert result.returncode == 0
+
+
+def test_example_2d_rand():
+    result = subprocess.run(
+        ['fast-conv', 'sim', ' rand'],
+        capture_output=True,
+        cwd=tmp_dir2
+    )
+    assert result.returncode == 0
+
+
+def test_show_1d():
+    result = subprocess.run(
+        ['fast-conv', 'show'],
+        capture_output=True,
+        cwd=tmp_dir1
+    )
+    assert result.returncode == 0
+
+
+def test_show_2d():
+    result = subprocess.run(
+        ['fast-conv', 'show'],
+        capture_output=True,
+        cwd=tmp_dir2
+    )
+    assert result.returncode == 0
