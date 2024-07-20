712e0b8e3fb3711037eb10a9733a9715f4bd8445
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2024-07-20 13:43:43 -0300

    working on fast conv 1d

--- src/fast_convolution/c/convolution.c
+++ src/fast_convolution/c/convolution.c
@@ -4,8 +4,8 @@
 
 #include "convolution.h"
 
-void naive_convolution(const int *weight, const int *feature, int *output, int f_row, int f_col, int w_row, int w_col,
-                       int out_col) {
+void naive_convolution(
+        const int *weight, const int *feature, int *output, int f_row, int f_col, int w_row, int w_col,int out_col) {
     int fi, fj, wi, wj;
     for (fi=0; fi < f_row - w_row + 1; fi++){
         for (fj=0; fj < f_col - w_col + 1; fj++){
@@ -18,7 +18,7 @@ void naive_convolution(const int *weight, const int *feature, int *output, int f
     }
 }
 
-void matrix_mul(const int *in1, const int *in2, int *out, int row1, int col2_row1, int col2) {
+void matrix_mul_int(const int *in1, const int *in2, int *out, int row1, int col2_row1, int col2) {
     int r, c, t;
     for (r=0; r < row1; r++) {
         for (c = 0; c < col2; c++) {
@@ -26,5 +26,30 @@ void matrix_mul(const int *in1, const int *in2, int *out, int row1, int col2_row
                 out[r * col2 + c] = out[r * col2 + c] + in1[r * col2_row1 + t] * in2[t * col2 + c];
             }
         }
+    }
+}
+
+void matrix_mul_float(const float *in1, const float *in2, float *out, int row1, int col2_row1, int col2) {
+    int r, c, t;
+    for (r=0; r < row1; r++) {
+        for (c = 0; c < col2; c++) {
+            for (t = 0; t < col2_row1; t++) {
+                out[r * col2 + c] = out[r * col2 + c] + in1[r * col2_row1 + t] * in2[t * col2 + c];
+            }
+        }
+    }
+}
+
+void hadamart_product_int(const int *in1, const int *in2, int *out, int row) {
+    int r;
+    for (r=0; r < row; r++) {
+        out[r] = in1[r] * in2[r];
+    }
+}
+
+void hadamart_product_float(const float *in1, const float *in2, float *out, int row) {
+    int r;
+    for (r=0; r < row; r++) {
+        out[r] = in1[r] * in2[r];
     }
 }
--- src/fast_convolution/c/fast-conv.c
+++ src/fast_convolution/c/fast-conv.c
@@ -7,42 +7,76 @@
 #define C_SIZE 5
 
 int main() {
-    const int m2[4*4] = {
-        -1, 0, 0, 0,
-        0, -1, 1, -1,
-        1, 1, 1, 0,
-        0, 0, 0, 1
-    };
-    const int mc[5*5] = {
-        2, -1,-2, 1,0,
-        0, -2, -1, 1, 0,
-        0, 2, -3, 1, 0,
-        0, -1, 0, 1, 0,
-        0, 2, -1, -2, 1,
-    };
-
-    const int mb[5*3] = {
+    const int mb[C_SIZE*A_SIZE] = {
             1, 0, 0,
             1, 1, 1,
             1, -1, 1,
             1, 2, 4,
             0, 0, 1,
     };
-
+    const int mc[C_SIZE*C_SIZE] = {
+        2, -1,-2, 1,0,
+        0, -2, -1, 1, 0,
+        0, 2, -3, 1, 0,
+        0, -1, 0, 1, 0,
+        0, 2, -1, -2, 1,
+    };
+    const float ma[A_SIZE*C_SIZE] = {
+        1, 1, 1, 1, 0,
+        0, 1, -1, 2, 0,
+        0, 1, 1, 4, 1,
+    };
     int md[C_SIZE] = {0, 1, 2, 3, 4};
     int mg[B_SIZE] = {0, 1, 2};
     float mq[C_SIZE] = {1/2, -1/2, -1/6, 1/6, 1};
     int mdd[C_SIZE] = {0};
-    int mgg[C_SIZE] = {0};
-    int ms[A_SIZE] = {0};
+    int mbg[C_SIZE] = {0};
+    float mgg[C_SIZE] = {0};
+    float mss[C_SIZE] = {0};
+    float ms[A_SIZE] = {0};
 
     int r;
-    int c;
-    matrix_mul(mc, md, mdd, C_SIZE, C_SIZE, 1);
 
+    // G=q.(b*g)
+    // bg=b*g
+    matrix_mul_int(mb, mg, mbg, C_SIZE, B_SIZE, 1);
+    printf("bg=b*g: ");
+    for (r=0; r < C_SIZE; r++) {
+        printf("%d\t", mbg[r]);
+    };
+    printf("\n");
+
+    // G=q.bg
+    hadamart_product_float((float *)mbg, mq, mgg, C_SIZE);
+    printf("G=q.bg: ");
+    for (r=0; r < C_SIZE; r++) {
+        printf("%f\t", mgg[r]);
+    };
+    printf("\n");
+
+    // D=ct*d
+    matrix_mul_int(mc, md, mdd, C_SIZE, C_SIZE, 1);
+    printf("D=ct*d: ");
     for (r=0; r < C_SIZE; r++) {
         printf("%d\t", mdd[r]);
-    }
+    };
+    printf("\n");
+
+    // S=D.G
+    hadamart_product_float((float *)mdd, mgg, mss, C_SIZE);
+    printf("S=D.G: ");
+    for (r=0; r < C_SIZE; r++) {
+        printf("%f\t", mss[r]);
+    };
+    printf("\n");
+
+    // s=S*a
+    matrix_mul_float(mss, ma, ms, A_SIZE, C_SIZE, 1);
+    printf("s=S*a: ");
+    for (r=0; r < B_SIZE; r++) {
+        printf("%f\t", ms[r]);
+    };
+    printf("\n");
 
     return 0;
 }
