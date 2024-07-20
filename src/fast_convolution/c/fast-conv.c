ba324163358c3a285d3be641207b70695d3d7067
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2024-07-20 14:06:08 -0300

    fast conv 1d is working with float

--- src/fast_convolution/c/fast-conv.c
+++ src/fast_convolution/c/fast-conv.c
@@ -26,9 +26,10 @@ int main() {
         0, 1, -1, 2, 0,
         0, 1, 1, 4, 1,
     };
-    float md[C_SIZE] = {0, 1, 2, 3, 4};
-    float mg[B_SIZE] = {0, 1, 2};
-    float mq[C_SIZE] = {1/2, -1/2, -1/6, 1/6, 1};
+    const float md[C_SIZE] = {0, 1, 2, 3, 4};
+    const float mg[B_SIZE] = {0, 1, 2};
+    const float mq[C_SIZE] = {1.0f/2.0f, -1.0f/2.0f, -1.0f/6.0f, 1.0f/6.0f, 1.0f};
+
     float mdd[C_SIZE] = {0};
     float mbg[C_SIZE] = {0};
     float mgg[C_SIZE] = {0};
@@ -42,15 +43,15 @@ int main() {
     matrix_mul_float(mb, mg, mbg, C_SIZE, B_SIZE, 1);
     printf("bg=b*g: ");
     for (r=0; r < C_SIZE; r++) {
-        printf("%f\t", mbg[r]);
+        printf("%.3f\t", mbg[r]);
     };
     printf("\n");
 
     // G=q.bg
-    hadamart_product_float(mbg, mq, mgg, C_SIZE);
+    hadamart_product_float(mq,mbg, mgg, C_SIZE);
     printf("G=q.bg: ");
     for (r=0; r < C_SIZE; r++) {
-        printf("%f\t", mgg[r]);
+        printf("%.3f\t", mgg[r]);
     };
     printf("\n");
 
@@ -58,7 +59,7 @@ int main() {
     matrix_mul_float(mc, md, mdd, C_SIZE, C_SIZE, 1);
     printf("D=ct*d: ");
     for (r=0; r < C_SIZE; r++) {
-        printf("%f\t", mdd[r]);
+        printf("%.3f\t", mdd[r]);
     };
     printf("\n");
 
@@ -66,15 +67,15 @@ int main() {
     hadamart_product_float((float *)mdd, mgg, mss, C_SIZE);
     printf("S=D.G: ");
     for (r=0; r < C_SIZE; r++) {
-        printf("%f\t", mss[r]);
+        printf("%.3f\t", mss[r]);
     };
     printf("\n");
 
     // s=S*a
-    matrix_mul_float(mss, ma, ms, A_SIZE, C_SIZE, 1);
+    matrix_mul_float(ma, mss, ms, A_SIZE, C_SIZE, 1);
     printf("s=S*a: ");
     for (r=0; r < B_SIZE; r++) {
-        printf("%f\t", ms[r]);
+        printf("%.3f\t", ms[r]);
     };
     printf("\n");
 
