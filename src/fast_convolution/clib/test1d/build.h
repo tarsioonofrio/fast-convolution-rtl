327fbd6bc9b68dafc16f4012e625dd1b5d75e8ca
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2024-07-24 09:05:32 -0300

    up

--- src/fast_convolution/clib/test1d/build.h
+++ src/fast_convolution/clib/test1d/build.h
@@ -1,32 +1,34 @@
-//
-// Created by tarsio on 21/07/2024.
-//
-
 #ifndef C_BUILD_H
 #define C_BUILD_H
 
-#include "config.h"
 
-const float mb[C_SIZE * A_SIZE] = {
-        1, 0, 0,
-        1, 1, 1,
-        1, -1, 1,
-        1, 2, 4,
-        0, 0, 1,
-};
-const float mc[C_SIZE * C_SIZE] = {
-        2, -1, -2, 1, 0,
-        0, -2, -1, 1, 0,
-        0, 2, -3, 1, 0,
-        0, -1, 0, 1, 0,
-        0, 2, -1, -2, 1,
-};
-const float ma[A_SIZE * C_SIZE] = {
-        1, 1, 1, 1, 0,
-        0, 1, -1, 2, 0,
-        0, 1, 1, 4, 1,
-};
-const float mq[C_SIZE] = {1.0f / 2.0f, -1.0f / 2.0f, -1.0f / 6.0f, 1.0f / 6.0f, 1.0f};
-
+const int mc[5*5] = {
+	2, 0, 0, 0, 0,
+	-1, -2, 2, -1, 2,
+	-2, -1, -3, 0, -1,
+	1, 1, 1, 1, -2,
+	0, 0, 0, 0, 1
+}
+const int mb[5*3] = {
+	1, 0, 0,
+	1, 1, 1,
+	1, -1, 1,
+	1, 2, 4,
+	0, 0, 1
+}
+const int ma[5*3] = {
+	1, 0, 0,
+	1, 1, 1,
+	1, -1, 1,
+	1, 2, 4,
+	0, 0, 1
+}
+const int mq[5*2] = {
+	1, 2,
+	-1, 2,
+	-1, 6,
+	1, 6,
+	1, 1
+}
 
 #endif //C_BUILD_H
--- src/fast_convolution/clib/test1d/example.h
+++ src/fast_convolution/clib/test1d/example.h
@@ -1,14 +1,18 @@
-//
-// Created by tarsio on 21/07/2024.
-//
-
 #ifndef C_EXAMPLE_H
 #define C_EXAMPLE_H
 
-#include "config.h"
 
-const float md[C_SIZE] = {0, 1, 2, 3, 4};
-const float mg[B_SIZE] = {0, 1, 2};
-
+const int md[5*1] = {
+	0,
+	1,
+	2,
+	3,
+	4
+}
+const int mg[3*1] = {
+	0,
+	1,
+	2
+}
 
 #endif //C_EXAMPLE_H
