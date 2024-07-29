9dd83ae32eee350c75df94b782cc2c55e5674083
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2024-07-29 19:39:26 -0300

    filter1d is not working

--- src/fast_convolution/clib/filter1d-float.c
+++ src/fast_convolution/clib/filter1d-float.c
@@ -1,23 +1,18 @@
-#include <stdio.h>
 #include "libs/convolution.h"
 #include "libs/util.h"
 #include "test1d/init.h"
-#include "test1d/build.h"
-#include "test1d/sim.h"
+#include "test1d/build_float.h"
+#include "test1d/sim_float.h"
 
 
 
 int main() {
     int i;
-    const int *mg;
+    const float *mg;
     float mgg[C_SIZE] = {};
 
-    float matf[A_SIZE * C_SIZE] = {0};
-    float mctf[C_SIZE * C_SIZE] = {0};
     float feat_out[FOUT_SIZE * FOUT_SIZE] = {0};
 
-    convert_int_to_float(mat, matf, C_SIZE * A_SIZE);
-    convert_int_to_float(mct, mctf, C_SIZE * C_SIZE);
 
     for (i=0; i < W_SIZE; i++) {
         mg = weight + W_SIZE*i;
