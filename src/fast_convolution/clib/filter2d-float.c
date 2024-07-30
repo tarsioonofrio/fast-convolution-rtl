1967754838cb0f7fc4146db308df79b42d3430e8
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2024-07-29 22:19:34 -0300

    working on filter2d

--- src/fast_convolution/clib/filter2d-float.c
+++ src/fast_convolution/clib/filter2d-float.c
@@ -1,23 +1,17 @@
-#include <stdio.h>
 #include "libs/convolution.h"
 #include "libs/util.h"
 #include "test2d/init.h"
-#include "test2d/build.h"
-#include "test2d/bind_nest.h"
-#include "test2d/example.h"
+#include "test2d/bind_nest_float.h"
+#include "test2d/sim_float.h"
 
 int main() {
     float feat_out[FOUT_SIZE * FOUT_SIZE] = {0};
-    float matf[A_SIZE * C_SIZE] = {0};
-    float mctf[C_SIZE * C_SIZE] = {0};
 
-    convert_int_to_float(mat, matf, C_SIZE * A_SIZE);
-    convert_int_to_float(mct, mctf, C_SIZE * C_SIZE);
+    filter2d_slide2d_float(feat_out, feat_in, mc_nest, ma_nest, weight_gg,
+                           A1_SIZE*A2_SIZE, C1_SIZE * C2_SIZE, FIN_SIZE, FOUT_SIZE);
 
-    to_bg(mgg, mq, mb, mg, B_SIZE, C_SIZE);
-
-    filter2d_slide2d_float(feat_out, feat_in, mctf, matf, mgg, A_SIZE, C_SIZE, FIN_SIZE, FOUT_SIZE);
     print_array2d_float(feat_out, FOUT_SIZE, FOUT_SIZE, "fout: ");
+    compare_array1d_float_to_int(gold, feat_out, FOUT_SIZE * FOUT_SIZE, "Errors in S != gold");
 
     return 0;
 }
