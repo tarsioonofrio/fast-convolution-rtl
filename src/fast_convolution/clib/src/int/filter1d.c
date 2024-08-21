e8aa750be10cfc488f956833d7727d33c615cc33
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2024-08-21 19:56:43 -0300

    up

--- src/fast_convolution/clib/src/int/filter1d.c
+++ src/fast_convolution/clib/src/int/filter1d.c
@@ -20,7 +20,7 @@ int main() {
 
     for (i=0; i < W_SIZE; i++) {
         mgg = weight_gg_quant + C_SIZE*i;
-        print_array1d(mgg, C_SIZE, "G: ");
+        // print_array1d(mgg, C_SIZE, "G: ");
         filter1d(feat_out, feat_in, i, mct, mat, mgg, A_SIZE,
                  C_SIZE, FIN_SIZE, FOUT_SIZE);
     }
