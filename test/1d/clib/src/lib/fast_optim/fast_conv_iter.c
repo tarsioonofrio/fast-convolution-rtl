89a3f9ee7100fda597ca47cc45b47176b81ee18a
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2024-09-14 20:44:09 -0300

    up

--- test/1d/clib/src/lib/fast_optim/fast_conv_iter.c
+++ test/1d/clib/src/lib/fast_optim/fast_conv_iter.c
@@ -16,16 +16,12 @@ void fast_conv(int *ms, const int *ma1t, const int *mc1t, const int *mgg,
     int *mss2 = (int *) malloc((a1_size * c1_size) * sizeof(int));
     int *mdd = (int *) malloc((c1_size * c2_size) * sizeof(int));
     int *md2 = (int *) malloc((c1_size * c2_size) * sizeof(int));
-    // int *ma2 = (int *) malloc((a2_size * c2_size) * sizeof(int));
-    // int *mc2 = (int *) malloc((c2_size * c2_size) * sizeof(int));
 
     init_array(ms, a1_size * a2_size);
     init_array(mss, c1_size * c2_size);
     init_array(mss2, a1_size * c1_size);
     init_array(mdd, c1_size * c2_size);
     init_array(md2, c1_size * c2_size);
-    // init_array(ma2, a2_size * c2_size);
-    // init_array(mc2, c2_size * c2_size);
 
     #ifdef __riscv
         csr_write_mcountinhibit(0);
@@ -34,7 +30,7 @@ void fast_conv(int *ms, const int *ma1t, const int *mc1t, const int *mgg,
     matrix_mul_shift_noloop_c2(md2, md);
     matrix_mul_shift_noloop_c1t(mdd, md2);
     hadamart_product_noloop_iter(mss, mdd, mgg);
-    matrix_mul_shift_noloop_a2(mss2, ma2);
+    matrix_mul_shift_noloop_a2(mss2, mss);
     matrix_mul_shift_noloop_a1t(ms, mss2);
 
     #ifdef __riscv
