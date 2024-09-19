85655a1c9ebf64429e3e5e2159c4272a003cbfe3
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2024-09-19 11:32:23 -0300

    up

--- etc/table-verilator.sh
+++ etc/table-verilator.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-OUTPUT="table.csv"
-FILES=$(find . -type f -name "report*.txt")
+ROOT=$1
+OUTPUT=$2
+FILES=$(find ROOT -type f -name "report*.txt")
 
 FIRST_FILE=$(echo "$FILES" | head -n 1)
 HEAD_FILE=$(dirname ${FIRST_FILE})/header.txt
@@ -12,9 +13,9 @@ for F in ${FILES}; do
     NAME=$(basename ${F})
     CUT_NAME=$(dirname ${F})/cut-${NAME}
     cut -c 26- ${F} > ${CUT_NAME}
-    sed -i "1i${G}" ${CUT_NAME}
+    sed -i "1i${F}" ${CUT_NAME}
 done
 
-FILES_CUT=$(find . -type f -name "cut-report*.txt")
+FILES_CUT=$(find ${ROOT} -type f -name "cut-report*.txt")
 
 paste -d ";"  ${HEAD_FILE} ${FILES_CUT} > ${OUTPUT}
