6bbb27e641adecd309f22d6d808473f5a3d83150
Társio Onofrio <tarsioonofrio@>
2024-10-22 17:11:44 -0300

    up

---
+++ test/2d.py
@@ -0,0 +1,33 @@
+import shutil
+from pathlib import Path
+
+import pytest
+
+from .conftest import file_list2d
+from .lib import read_json, run
+
+root = Path(__file__).parent.resolve()
+
+list_repo_path = [root / (Path(f).stem) for f in file_list2d]
+list_repo_opt = [["-p", f.as_posix()] for f in list_repo_path]
+
+list_cmd_dict = [
+    read_json(root / "json" / (f.stem + ".json")) for f in list_repo_path
+]
+
+list_args = [
+    (x, y, z) for x, y, z in zip(list_repo_path, list_repo_opt, list_cmd_dict)
+]
+
+
+@pytest.mark.parametrize("args", list_args)
+def test_rm(args):
+    repo_path, repo_opt, cmd_dict = args
+    shutil.rmtree(repo_path, ignore_errors=True)
+
+
+@pytest.mark.parametrize("args", list_args)
+@pytest.mark.parametrize("cmd", ["init", "build", "bind"])
+def test_project(args, cmd):
+    repo_path, repo_opt, cmd_dict = args
+    run(cmd, repo_path, repo_opt, cmd_dict)
