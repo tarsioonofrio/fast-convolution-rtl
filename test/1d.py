2ad31bf4198efe9db56cc085a73c4a94719a76cb
Társio Onofrio <tarsioonofrio@>
2024-10-22 11:46:20 -0300

    up

--- test/1d.py
+++ test/1d.py
@@ -8,6 +8,7 @@ from click.testing import CliRunner
 from fast_convolution import cli
 
 from .conftest import file_list
+from .lib import run
 
 # @pytest.fixture(scope="session")
 # def file(pytestconfig):
@@ -21,16 +22,11 @@ def read_json(file):
 
 root = Path(__file__).parent.resolve()
 
-open(root / "json/cmd_common.json")
-
 with open(root / "json/cmd_common.json") as f:
     cmd_common_dict = json.load(f)
 
 list_repo_path = [root / (Path(f).stem) for f in file_list]
 list_repo_opt = [["-p", f.as_posix()] for f in list_repo_path]
-
-# with open(root / "json" / Path(__file__).with_suffix(".json").name) as f:
-#     cmd_dict = json.load(f)
 
 list_cmd_dict = [
     read_json(root / "json" / (f.stem + ".json")) for f in list_repo_path
@@ -40,71 +36,64 @@ list_args = [
     (x, y, z) for x, y, z in zip(list_repo_path, list_repo_opt, list_cmd_dict)
 ]
 
+
 @pytest.mark.parametrize("args", list_args)
 def test_init(args):
     repo_path, repo_opt, cmd_dict = args
     shutil.rmtree(repo_path, ignore_errors=True)
-    runner = CliRunner()
-    result = runner.invoke(cli.main, repo_opt + cmd_dict["init"])
-    assert result.exit_code == 0
+    run("init", repo_path, repo_opt, cmd_dict)
 
 
 @pytest.mark.parametrize("args", list_args)
 def test_build(args):
-    print(args)
     repo_path, repo_opt, cmd_dict = args
-    runner = CliRunner()
-    result = runner.invoke(cli.main, repo_opt + cmd_dict["build"])
-    assert result.exit_code == 0
+    run("build", repo_path, repo_opt, cmd_dict)
 
 
-# def test_bind():
+# def test_bind(args):
+#     repo_path, repo_opt, cmd_dict = args
 #     runner = CliRunner()
 #     result = runner.invoke(cli.main, repo_opt + cmd_dict["bind"])
 #     assert result.exit_code == 0
 
 
-# def test_ex_rand():
-#     runner = CliRunner()
-#     result = runner.invoke(cli.main, repo_opt + cmd_common_dict["ex_rand"])
-#     assert result.exit_code == 0
+@pytest.mark.parametrize("args", list_args)
+def test_ex_rand(args):
+    repo_path, repo_opt, cmd_dict = args
+    run("ex_rand", repo_path, repo_opt, cmd_common_dict)
 
 
-# def test_ex_seq():
-#     runner = CliRunner()
-#     result = runner.invoke(cli.main, repo_opt + cmd_common_dict["ex_seq"])
-#     assert result.exit_code == 0
+@pytest.mark.parametrize("args", list_args)
+def test_ex_seq(args):
+    repo_path, repo_opt, cmd_dict = args
+    run("ex_seq", repo_path, repo_opt, cmd_common_dict)
 
 
-# def test_sim_rand():
-#     runner = CliRunner()
-#     result = runner.invoke(cli.main, repo_opt + cmd_common_dict["sim_rand"])
-#     assert result.exit_code == 0
+@pytest.mark.parametrize("args", list_args)
+def test_sim_rand(args):
+    repo_path, repo_opt, cmd_dict = args
+    run("sim_rand", repo_path, repo_opt, cmd_common_dict)
 
 
-# def test_sim_file():
-#     runner = CliRunner()
-#     result = runner.invoke(cli.main, repo_opt + cmd_common_dict["sim_file"])
-#     assert result.exit_code == 0
+@pytest.mark.parametrize("args", list_args)
+def test_sim_file(args):
+    repo_path, repo_opt, cmd_dict = args
+    run("sim_file", repo_path, repo_opt, cmd_common_dict)
 
 
-# def test_quant():
-#     runner = CliRunner()
-#     result = runner.invoke(cli.main, repo_opt + cmd_common_dict["quant"])
-#     assert result.exit_code == 0
+@pytest.mark.parametrize("args", list_args)
+def test_quant(args):
+    repo_path, repo_opt, cmd_dict = args
+    run("quant", repo_path, repo_opt, cmd_common_dict)
 
 
-# def test_sim_rand_quant():
-#     runner = CliRunner()
-#     result = runner.invoke(
-#         cli.main, repo_opt + cmd_common_dict["sim_rand_quant"]
-#     )
-#     assert result.exit_code == 0
+@pytest.mark.parametrize("args", list_args)
+def test_sim_rand_quant(args):
+    repo_path, repo_opt, cmd_dict = args
+    run("sim_rand_quant", repo_path, repo_opt, cmd_common_dict)
 
 
-# def test_sim_file_quant():
-#     runner = CliRunner()
-#     result = runner.invoke(
-#         cli.main, repo_opt + cmd_common_dict["sim_file_quant"]
-#     )
-#     assert result.exit_code == 0
+@pytest.mark.parametrize("args", list_args)
+def test_sim_file_quant(args):
+    repo_path, repo_opt, cmd_dict = args
+    run("sim_file_quant", repo_path, repo_opt, cmd_common_dict)
