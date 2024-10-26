cc7dcf80dd92108bb219699a17bb9c11580ecff1
Társio Onofrio <tarsioonofrio@>
2024-10-25 22:30:11 -0300

    add 2d manual nest

---
+++ test/2d-manual-nest/clib/make/filter2d-nest-opt/lib/include/optim.h
@@ -0,0 +1,14 @@
+#ifndef C_OPTIM_H
+#define C_OPTIM_H
+
+void matrix_mul_shift_noloop_c2(int *m_out, const int *m_in);
+
+void matrix_mul_shift_noloop_c1t(int *m_out, const int *m_in);
+
+void matrix_mul_shift_noloop_a2(int *m_out, const int *m_in);
+
+void matrix_mul_shift_noloop_a1t(int *m_out, const int *m_in);
+
+void hadamart_product_noloop_nest(int *out, const int *in1, const int *in2);
+
+#endif //C_OPTIM_H
