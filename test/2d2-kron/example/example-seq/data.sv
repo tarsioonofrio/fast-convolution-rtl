52c562b9347e0ae7b43c2a0bba1bd4f2af240a7d
Társio Onofrio <tarsioonofrio@>
2025-03-31 14:42:18 -0300

    up

--- test/2d2-kron/example/example-seq/data.sv
+++ test/2d2-kron/example/example-seq/data.sv
@@ -1,16 +1,23 @@
 package data;
 
-const int md = {
-	'{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
-};
-const int mg = {
-	'{0, 1, 2, 3, 4, 5, 6, 7, 8
-};
-const int mgg = {
-	'{0, -1, 0, -2, -4, 9, 3, 7, -1, 3, 1, 2, -6, 10, 3, 8
-};
-const int ms = {
-	'{258, 294, 402, 438
-};
+  `define A1_SIZE 2
+  `define B1_SIZE 3
+  `define C1_SIZE 4
+  `define M1_SIZE 4
+  `define A2_SIZE 2
+  `define B2_SIZE 3
+  `define C2_SIZE 4
+  `define M2_SIZE 4
+
+  const int feat_in[][] = {
+    '{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
+  };
+  const int weight[][] = {
+    '{0, -1, 0, -2, -4, 9, 3, 7, -1, 3, 1, 2, -6, 10, 3, 8},
+  };
+  const int feat_out[][] = {
+    '{258, 294, 402, 438},
+  };
+
 
 endpackage
