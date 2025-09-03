4f0d2b2618a89ecf8520b171ae554e274de63588
Társio Onofrio <tarsioonofrio@>
2025-09-03 12:07:08 -0300

    up

--- copy-sv.sh
+++
@@ -1,8 +0,0 @@
-shopt -s nullglob
-for svdir in ../fast-convolution-rtl/test/*/sv/; do
-  var=${svdir#test/}; var=${var%/sv/}   # extrai o nome entre test/ e /sv/
-  dst="./rtl/mux-mult/$var/"
-  mkdir -p "$dst"
-  rsync -av "$svdir" "$dst"
-done
-shopt -u nullglob
