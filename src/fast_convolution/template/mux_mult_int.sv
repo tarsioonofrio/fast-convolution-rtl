0b4563ef6483eed068096ce8ce0e7d92bbfcd4e6
Társio Onofrio <tarsioonofrio@>
2025-09-06 18:21:19 -0300

    Add MuxMult module and rename mux_mult.sv to mux_mult_state.sv

---
+++ src/fast_convolution/template/mux_mult_int.sv
@@ -0,0 +1,19 @@
+//-------------------------------------------------------------------------
+// Index multiplexer module for selecting register indices based on state
+//-------------------------------------------------------------------------
+
+module MuxMult
+  import packConv::*;
+  (
+    input  logic[6:0] idx_in, // current state
+    output logic[6:0] idx_out[0:NMULT-1]  // index array output
+  );
+
+  always_comb begin
+    unique case (current_st)
+{case}
+    default: begin end
+    endcase
+  end
+
+endmodule
