a97d9dc1b7e68e994e1287f126bd6ec5d0908467
Társio Onofrio <tarsioonofrio@>
2025-09-16 02:11:21 -0300

    Use $clog2 to size MuxMult index signals correctly

--- test/2d-tcn16/sv/mux_mult_04.sv
+++ test/2d-tcn16/sv/mux_mult_04.sv
@@ -10,8 +10,8 @@ endpackage
 
 module MuxMult
   (
-    input  logic[5:0] idx_in, // current state
-    output logic[5:0] idx_out[0:4 - 1]  // index array output
+    input  logic[$clog2(4):0] idx_in, // current state
+    output logic[$clog2(4):0] idx_out[0:4 - 1]  // index array output
   );
 
   timeunit 1ns;
